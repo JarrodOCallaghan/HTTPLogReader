d427ddd6e43f6ea8ad6c5ba69d106ae29b1bf637
Jarrod O'callaghan <jarrod@Jarrods-MacBook-Pro.local>
2024-07-02 14:01:06 +1000

    Added initial report requirements

--- app/src/main/java/ocallaghan/jarrod/httplogreader/App.java
+++ app/src/main/java/ocallaghan/jarrod/httplogreader/App.java
@@ -3,15 +3,21 @@ package ocallaghan.jarrod.httplogreader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args){
-        HashMap<String, Integer> ipAddresses = new HashMap<String, Integer>();
-        HashMap<String, Integer> urlAccessed = new HashMap<String, Integer>();
-        String[] top3IP;
-        String[] top3URL; 
+        Map<String, Integer> ipAddresses = new HashMap<String, Integer>();
+        Map<String, Integer> urlAccessed = new HashMap<String, Integer>();
 
+        // Lets store the top 3 items
         try {
             File file = new File("/Users/jarrod/Repositories/HTTPLogReader/SampleData/data.log");
             Scanner scanner = new Scanner(file);
@@ -35,8 +41,6 @@ public class App {
                 } else {
                     urlAccessed.put(url, 1);
                 }
-
-
             }
             scanner.close();
         } catch (FileNotFoundException e) {
@@ -47,11 +51,61 @@ public class App {
             System.out.println("Unable to parse file, it may not be the correct format");
             System.exit(0);
         }
-        // System.out.println(ipAddresses);
-        // hashmap.size() is O(1) time
+
         System.out.println("Unique IP Addresses: " + ipAddresses.size());
 
-        System.out.println(urlAccessed);
+        //Going to now 'flip' the ip list so it can be ordered
+        SortedMap<Integer, HashSet<String>> sortedIPList = new TreeMap<>();
+
+        ipAddresses.forEach((ip, count) -> {
+            if (sortedIPList.containsKey(count)){
+                sortedIPList.get(count).add(ip);
+            } else {
+                HashSet<String> tmp = new HashSet<String>();
+                tmp.add(ip);
+                sortedIPList.put(count, tmp);
+            }
+        });
+
+        // Top 3 IP Addresses
+        SortedMap<Integer, HashSet<String>> reversedIPList = sortedIPList.reversed();
+        Integer reversedIPListLength = reversedIPList.size();
+        System.out.println(reversedIPListLength);
+        Integer topIPLen = (reversedIPListLength < 3) ? reversedIPListLength: 3;
+        System.out.println("The top " + topIPLen + " most active IP addresses by occurrence");
+        for (int i = 0; i < reversedIPListLength && i < 2 ; i ++){
+            System.out.println("Occurrences: " + reversedIPList.firstKey() + " - " + reversedIPList.get(reversedIPList.firstKey()));
+            reversedIPList.remove(reversedIPList.firstKey());
+        }
+
+
+        // URL's
+        SortedMap<Integer, HashSet<String>> sortedURLList = new TreeMap<>();
+
+        urlAccessed.forEach((url, count) -> {
+            if (sortedURLList.containsKey(count)){
+                sortedURLList.get(count).add(url);
+            } else {
+                HashSet<String> tmp = new HashSet<String>();
+                tmp.add(url);
+                sortedURLList.put(count, tmp);
+            }
+        });
+
+        // Top 3 IP Addresses
+        SortedMap<Integer, HashSet<String>> reversedURLList = sortedURLList.reversed();
+        Integer reversedURLListLength = reversedURLList.size();
+        Integer topURLLen = (reversedURLListLength < 3) ? reversedURLListLength: 3;
+        System.out.println("The top " + topURLLen + " most visited URLs");
+        for (int i = 0; i < reversedURLListLength && i < 2 ; i ++){
+            System.out.println("Occurrences: " + reversedURLList.firstKey() + " - " + reversedURLList.get(reversedURLList.firstKey()));
+            reversedURLList.remove(reversedURLList.firstKey());
+        }
+
+
+        
+
 
     }
+
 }
